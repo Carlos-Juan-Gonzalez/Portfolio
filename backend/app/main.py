41939a11502e851945392fa526a9533ead2b4ac5
Carlos-Juan-Gonzalez <carlosjuangonzalez99@gmail.com>
2026-05-14 08:28:57 +0200

    feat: add Docker setup for frontend and backend with docker-compose integration, Claude Api connection not implemented yet

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app.include_router(chat.router)
 # CORS provisional
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:8000"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
